f4a42b5530c19cea63b3e59f79d5c985ab5f3959
Jörn Röder <kontakt@joernroeder.de>
2014-04-21 14:24:10 +0200

    node webkit tray initial commit

--- src/main.js
+++ src/main.js
@@ -1,15 +1,11 @@
 /**
- * Created by joernroeder on 4/21/14.
- */
-
+* Created by joernroeder on 4/21/14.
+*/
 /// <reference path='./main.d.ts' />
-
-var gui = require('nw.gui'),
-    tray = new gui.Tray({
-        title: 'Tray',
-        icon: 'icon.png'
-    }),
-    menu = new gui.Menu();
+var gui = require('nw.gui'), tray = new gui.Tray({
+    title: 'Tray',
+    icon: 'icon.png'
+}), menu = new gui.Menu();
 
 // Give it a menu
 menu.append(new gui.MenuItem({
@@ -23,9 +19,10 @@ menu.append(new gui.MenuItem({
 menu.append(new gui.MenuItem({
     label: 'Quit'
 }));
-menu.items[menu.items.length - 1].click = function() {
+menu.items[menu.items.length - 1].click = function () {
     console.log('bye bye');
     gui.App.quit();
 };
 
 tray.menu = menu;
+//# sourceMappingURL=main.js.map
