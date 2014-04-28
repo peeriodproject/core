7da59c2d3147a56ba959d3a8b69658215d46f0e8
Jörn Röder <kontakt@joernroeder.de>
2014-04-28 16:07:20 +0200

    added Config interfaces, JSONConfig class and options

--- src/core/config/config.js
+++ src/core/config/config.js
@@ -1,1 +1,80 @@
-//# sourceMappingURL=config.js.map
+/// <reference path='Config.d.ts' />
+/// <reference path='../../../ts-definitions/node/node.d.ts' />
+/**
+* The class `JSONConfig` loads a JSON-file and converts it into a dot.notated key-value store.
+*
+* @class config.JSONConfig
+* @implements config.ConfigInterface
+*
+* @param {string} configPath
+* @param {config.ConfigKeyList} keys
+*/
+var JSONConfig = (function () {
+    function JSONConfig(configPath, keys) {
+        if (typeof keys === "undefined") { keys = []; }
+        /**
+        * @private
+        * @member {config.ConfigPairList} config.JSONConfig#_data
+        */
+        this._data = [];
+        if (configPath.indexOf('.json') === -1) {
+            configPath += '.json';
+        }
+
+        var fileData = require(configPath);
+
+        this._data = this._convertToDotNotation(fileData, keys);
+    }
+    /**
+    * @private
+    * @method config.JSONConfig#_convertToDotNotation
+    *
+    * @param {Object} obj
+    * @param {config.ConfigKeyList} configKeys
+    * @returns {config.ConfigPairList} The dot-notated key-value object
+    */
+    JSONConfig.prototype._convertToDotNotation = function (obj, configKeys) {
+        var res = {}, recurse = function (obj, configKeys, current) {
+            for (var key in obj) {
+                var value = obj[key], newKey = (current ? current + '.' + key : key);
+
+                if (value && typeof value === 'object') {
+                    recurse(value, configKeys, newKey); // it's a nested object, so do it again
+                } else if (configKeys.length) {
+                    for (var i in configKeys) {
+                        // the key starts with the given config key, so add the property
+                        if (newKey.indexOf(configKeys[i]) === 0) {
+                            res[newKey.toLowerCase()] = value;
+                            break;
+                        }
+                    }
+                } else {
+                    res[newKey] = value;
+                }
+            }
+        };
+
+        recurse(obj, configKeys);
+
+        return Object.freeze(res);
+    };
+
+    JSONConfig.prototype.get = function (key, alternative) {
+        if (!key) {
+            throw new Error('Config.get: No config key given.');
+        }
+
+        key = key.toLowerCase();
+
+        if (this._data[key] !== undefined) {
+            return this._data[key];
+        } else if (alternative !== undefined) {
+            return alternative;
+        }
+
+        throw new Error('Config: no value for "' + key + '" found.');
+    };
+    return JSONConfig;
+})();
+exports.JSONConfig = JSONConfig;
+//# sourceMappingURL=Config.js.map
