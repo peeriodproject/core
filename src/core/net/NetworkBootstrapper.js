b52041d7bcf70d6fc15cd93030de2071e77b635b
Jonathan Pirnay <mail@johnnycrab.com>
2014-05-03 12:51:15 +0200

    network bootstrapped done (not commented yet, not tested yet)

--- src/core/net/NetworkBootstrapper.js
+++ src/core/net/NetworkBootstrapper.js
@@ -1,10 +1,73 @@
+var TCPSocketHandler = require('./tcp/TCPSocketHandler');
+
 /**
 * foobar
 * @class Test
 */
 var NetworkBootstrapper = (function () {
-    function NetworkBootstrapper(config) {
+    function NetworkBootstrapper(config, ipObtainers) {
+        this._externalIp = '';
+        this._config = null;
+        this._tcpSocketHandler = null;
+        this._config = config;
+        this._ipObtainers = ipObtainers;
     }
+    NetworkBootstrapper.prototype.bootstrap = function (callback) {
+        var _this = this;
+        this._getExternalIp(function (err, ip) {
+            if (err) {
+                callback(err);
+            } else {
+                _this._externalIp = ip;
+
+                _this._tcpSocketHandler = new TCPSocketHandler(_this._getTCPSocketHandlerOptions());
+
+                _this._tcpSocketHandler.autoBootstrap(function (openPorts) {
+                    callback(null);
+                });
+            }
+        });
+    };
+
+    NetworkBootstrapper.prototype.getTCPSocketHandler = function () {
+        return this._tcpSocketHandler;
+    };
+
+    NetworkBootstrapper.prototype._getExternalIp = function (callback) {
+        var _this = this;
+        if (!(this._ipObtainers && this._ipObtainers.length > 0)) {
+            callback(new Error('NetworkBootstrapper: No IP obtainers specified.'), null);
+
+            return;
+        }
+
+        var index = 0;
+        var doObtain = function (i) {
+            _this._ipObtainers[i].obtainIP(obtainCallback);
+        };
+        var obtainCallback = function (err, ip) {
+            // got an error, but there's still other obtainers
+            if (err && index < (_this._ipObtainers.length - 1)) {
+                doObtain(++index);
+            } else if (err) {
+                callback(new Error('NetworkBootstrapper: All IP obtainers throw an error.'), null);
+            } else {
+                callback(null, ip);
+            }
+        };
+
+        doObtain(index);
+    };
+
+    NetworkBootstrapper.prototype._getTCPSocketHandlerOptions = function () {
+        return {
+            allowHalfOpenSockets: this._config.get('net.allowHalfOpenSockets'),
+            connectionRetry: this._config.get('net.connectionRetrySeconds'),
+            idleConnectionKillTimeout: this._config.get('net.idleConnectionKillTimeout'),
+            myExternalIp: this._externalIp,
+            myOpenPorts: [this._config.get('net.myOpenPorts.0')]
+        };
+    };
     return NetworkBootstrapper;
 })();
 
--- src/core/net/NetworkBootstrapper.ts
+++ src/core/net/NetworkBootstrapper.ts
@@ -1,4 +1,8 @@
 import NetworkBootstrapperInterface = require('./interfaces/NetworkBootstrapperInterface');
+import TCPSocketHandlerOptions = require('./tcp/interfaces/TCPSocketHandlerOptions');
+import TCPSocketHandlerInterface = require('./tcp/interfaces/TCPSocketHandlerInterface');
+import TCPSocketHandler = require('./tcp/TCPSocketHandler');
+import ExternalIPObtainerInterface = require('./ip/interfaces/ExternalIPObtainerInterface');
 import ConfigInterface = require('../config/interfaces/ConfigInterface');
 
 /**
@@ -6,9 +10,78 @@ import ConfigInterface = require('../config/interfaces/ConfigInterface');
  * @class Test
  */
 class NetworkBootstrapper implements NetworkBootstrapperInterface {
+	private _externalIp:string = '';
 
-	constructor(config:ConfigInterface) {
+	private _config:ConfigInterface = null;
 
+	private _ipObtainers:Array<ExternalIPObtainerInterface>;
+
+	private _tcpSocketHandler:TCPSocketHandlerInterface = null;
+
+	constructor (config:ConfigInterface, ipObtainers:Array<ExternalIPObtainerInterface>) {
+		this._config = config;
+		this._ipObtainers = ipObtainers;
+	}
+
+	public bootstrap (callback:(err:Error) => any):void {
+		this._getExternalIp((err:Error, ip:string) => {
+			if (err) {
+				callback(err);
+			}
+			else {
+				this._externalIp = ip;
+
+				this._tcpSocketHandler = new TCPSocketHandler(this._getTCPSocketHandlerOptions());
+
+				this._tcpSocketHandler.autoBootstrap(function (openPorts:Array<number>) {
+					callback(null);
+				});
+			}
+
+		});
+	}
+
+	public getTCPSocketHandler ():TCPSocketHandlerInterface {
+		return this._tcpSocketHandler;
+	}
+
+	private _getExternalIp (callback:(err:Error, ip:string) => any):void {
+		if (!(this._ipObtainers && this._ipObtainers.length > 0)) {
+			callback(new Error('NetworkBootstrapper: No IP obtainers specified.'), null);
+
+			return;
+		}
+
+		var index:number = 0;
+		var doObtain:Function = (i) => {
+			this._ipObtainers[i].obtainIP(obtainCallback);
+		};
+		var obtainCallback = (err:Error, ip:string) => {
+			// got an error, but there's still other obtainers
+			if (err && index < (this._ipObtainers.length - 1)) {
+				doObtain(++index);
+			}
+			// got an error, and all obtainers are exhausted
+			else if (err) {
+				callback(new Error('NetworkBootstrapper: All IP obtainers throw an error.'), null);
+			}
+			// got an ip
+			else {
+				callback(null, ip);
+			}
+		};
+
+		doObtain(index);
+	}
+
+	private _getTCPSocketHandlerOptions ():TCPSocketHandlerOptions {
+		return <TCPSocketHandlerOptions> {
+			allowHalfOpenSockets     : this._config.get('net.allowHalfOpenSockets'),
+			connectionRetry          : this._config.get('net.connectionRetrySeconds'),
+			idleConnectionKillTimeout: this._config.get('net.idleConnectionKillTimeout'),
+			myExternalIp             : this._externalIp,
+			myOpenPorts              : [this._config.get('net.myOpenPorts.0')]
+		};
 	}
 
 }
