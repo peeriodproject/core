2b90195d0554fc71868e78eb329d5b0089cf78f4
Jonathan Pirnay <mail@johnnycrab.com>
2014-05-05 13:05:28 +0200

    comments

--- src/core/protocol/messages/ReadableMessage.ts
+++ src/core/protocol/messages/ReadableMessage.ts
@@ -14,6 +14,10 @@ import MessageByteCheatsheet = require('./MessageByteCheatsheet');
 /**
  * @class core.protocol.messages.ReadableMessage
  * @implements core.protocol.messages.ReadableMessageInterface
+ *
+ * @param {Buffer} buffer The message buffer
+ * @param {core.topology.ContactNodeFactoryInterface} nodeFactory A contact node factory
+ * @param {core.topology.ContactNodeAddressFactoryInterface} addressFactory An address factory.
  */
 class ReadableMessage implements ReadableMessageInterface {
 
@@ -23,21 +27,29 @@ class ReadableMessage implements ReadableMessageInterface {
 	private _addressFactory:ContactNodeAddressFactoryInterface = null;
 
 	/**
+	 * The message buffer.
+	 *
 	 * @member {Buffer} core.protocol.messages.ReadableMessage~_buffer
 	 */
 	private _buffer:Buffer = null;
 
 	/**
+	 * Length of the message buffer.
+	 *
 	 * @member {number} core.protocol.messages.ReadableMessage~_bufferLength
 	 */
 	private _bufferLength:number = 0;
 
 	/**
+	 * A helper member for remembering which bytes have already been read and processed.
+	 *
 	 * @member {number}  core.protocol.messages.ReadableMessage~_lastPosRead
 	 */
 	private _lastPosRead:number = 0;
 
 	/**
+	 * The type of protocol message (e.g. PING, PONG, etc.)
+	 *
 	 * @member {string} core.protocol.messages.ReadableMessage~_messageType
 	 */
 	private _messageType:string = null;
@@ -48,16 +60,22 @@ class ReadableMessage implements ReadableMessageInterface {
 	private _nodeFactory:ContactNodeFactoryInterface = null;
 
 	/**
+	 * The slice of message buffer constituting the payload of the message.
+	 *
 	 * @member {Buffer} core.protocol.messages.ReadableMessage~_payload
 	 */
 	private _payload:Buffer = null;
 
 	/**
+	 * The ID of the intended receiver of the message.
+	 *
 	 * @member {core.topology.IdInterface} core.protocol.messages.ReadableMessage~_receiverId
 	 */
 	private _receiverId:IdInterface = null;
 
 	/**
+	 * The sender object.
+	 *
 	 * @member {core.topology.ContactNodeInterface} core.protocol.messages.ReadableMessage~_sender
 	 */
 	private _sender:ContactNodeInterface = null;
@@ -71,6 +89,69 @@ class ReadableMessage implements ReadableMessageInterface {
 		this._deformat();
 	}
 
+	public discard ():void {
+		this._buffer = null;
+		this._payload = null;
+	}
+
+	public getMessageType ():string {
+		return this._messageType;
+	}
+
+	public getPayload ():Buffer {
+		return this._payload;
+	}
+
+	public getReceiverId ():IdInterface {
+		return this._receiverId;
+	}
+
+	public getSender ():ContactNodeInterface {
+		return this._sender;
+	}
+
+	/**
+	 * Makes a ContactNodeAddress out of a buffer representing an IPv4 address.
+	 *
+	 * @method core.protocol.messages.ReadableMessage~_contactNodeAddressByIPv4Buffer
+	 *
+	 * @param {Buffer} buffer
+	 * @returns {ContactNodeAddressInterface}
+	 */
+	private _contactNodeAddressByIPv4Buffer (buffer:Buffer):ContactNodeAddressInterface {
+		var ip:string = buffer.slice(0, 4).toJSON().join('.');
+		var port:number = buffer.readUInt16BE(4);
+
+		return this._addressFactory.create(ip, port);
+	}
+
+	/**
+	 * Makes a ContactNodeAddress out of a buffer representing an IPv6 address.
+	 *
+	 * @method core.protocol.messages.ReadableMessage~_contactNodeAddressByIPv6Buffer
+	 *
+	 * @param {Buffer} buffer
+	 * @returns {ContactNodeAddressInterface}
+	 */
+	private _contactNodeAddressByIPv6Buffer (buffer:Buffer):ContactNodeAddressInterface {
+		var ip:string = '';
+		var port:number = buffer.readUInt16BE(16);
+
+		for (var i=0; i<8; i++) {
+			ip += buffer.slice(i * 2, i*2 + 2).toString('hex');
+			if (i !== 7) {
+				ip += ':';
+			}
+		}
+
+		return this._addressFactory.create(ip, port);
+	}
+
+	/**
+	 * Kicks off the extracting process.
+	 *
+	 * @method core.protocol.messages.ReadableMessage~_deformat
+	 */
 	private _deformat ():void {
 		if (!this._isProtocolMessage()) {
 			throw new Error('ReadableMessage~_deformat: Buffer is not protocol compliant.');
@@ -88,48 +169,14 @@ class ReadableMessage implements ReadableMessageInterface {
 
 	}
 
-	public discard ():void {
-		this._buffer = null;
-		this._payload = null;
-	}
-
-	public getReceiverId ():IdInterface {
-		return this._receiverId;
-	}
-
-	public getSender ():ContactNodeInterface {
-		return this._sender;
-	}
-
-	public getMessageType ():string {
-		return this._messageType;
-	}
-
-	public getPayload ():Buffer {
-		return this._payload;
-	}
-
-	private _contactNodeAddressByIPv4Buffer (buffer:Buffer):ContactNodeAddressInterface {
-		var ip:string = buffer.slice(0, 4).toJSON().join('.');
-		var port:number = buffer.readUInt16BE(4);
-
-		return this._addressFactory.create(ip, port);
-	}
-
-	private _contactNodeAddressByIPv6Buffer (buffer:Buffer):ContactNodeAddressInterface {
-		var ip:string = '';
-		var port:number = buffer.readUInt16BE(16);
-
-		for (var i=0; i<8; i++) {
-			ip += buffer.slice(i * 2, i*2 + 2).toString('hex');
-			if (i !== 7) {
-				ip += ':';
-			}
-		}
-
-		return this._addressFactory.create(ip, port);
-	}
-
+	/**
+	 * Extracts a 20 byte ID from the message buffer.
+	 *
+	 * @method core.protocol.messages.ReadableMessage~_extractId
+	 *
+	 * @param {number} from Byte index to start from
+	 * @returns {Id} The created ID
+	 */
 	private _extractId (from:number):IdInterface {
 		var idBuffer = new Buffer(20);
 
@@ -138,11 +185,27 @@ class ReadableMessage implements ReadableMessageInterface {
 		return new Id(idBuffer, 160)
 	}
 
+	/**
+	 * Extracts the slice of the message buffer representing the message payload.
+	 *
+	 * @method core.protocol.messages.ReadableMessage~_extractPayload
+	 *
+	 * @param {number} from Byte index to start from
+	 * @returns {number} Index of last byte read.
+	 */
 	private _extractPayload (from:number):number {
 		this._payload = this._buffer.slice(from, this._buffer.length - MessageByteCheatsheet.messageEnd.length);
 		return from + this._payload.length;
 	}
 
+	/**
+	 * Extracts the protocol message type.
+	 *
+	 * @method core.protocol.messages.ReadableMessage~_extractMessageType
+	 *
+	 * @param {number} from Byte index to start from
+	 * @returns {number} The index of the last byte read
+	 */
 	private _extractMessageType (from:number):number {
 		var msgTypeBytes:Buffer = this._buffer.slice(from, from + 2);
 		var messageTypes = MessageByteCheatsheet.messageTypes;
@@ -168,12 +231,28 @@ class ReadableMessage implements ReadableMessageInterface {
 		return from + 2;
 	}
 
+	/**
+	 * Extracts the ID of the intended receiver.
+	 *
+	 * @method core.protocol.messages.ReadableMessage~_extractReceiverId
+	 *
+	 * @param {number} from The byte index to start from
+	 * @returns {number} The index of the last byte read
+	 */
 	private _extractReceiverId (from:number):number {
 		this._receiverId = this._extractId(from);
 
 		return from + 20;
 	}
 
+	/**
+	 * Extract the sender addresses and returns them in an array
+	 *
+	 * @method core.protocol.messages.ReadableMessage~_extractSenderAddressesAndBytesReadAsArray
+	 *
+	 * @param {number} from The index of bytes to start from
+	 * @returns {Array} Returns an array with two items: First is the array of the sender's addresses, second is the index of the last byte read.
+	 */
 	private _extractSenderAddressesAndBytesReadAsArray (from):any {
 		var doRead = true;
 		var result:ContactNodeAddressListInterface = [];
@@ -207,6 +286,15 @@ class ReadableMessage implements ReadableMessageInterface {
 		return [result, from];
 	}
 
+
+	/**
+	 * Extracts the sender ID and address block and makes ContactNode out of it.
+	 *
+	 * @method core.protocol.messages.ReadableMessage~_extractSenderAsContactNode
+	 *
+	 * @param {number} from Byte index to start from.
+	 * @returns {number} Index of the last byte read.
+	 */
 	private _extractSenderAsContactNode (from:number):number {
 		var senderId:IdInterface = this._extractId(from);
 
@@ -220,6 +308,13 @@ class ReadableMessage implements ReadableMessageInterface {
 		return res[1];
 	}
 
+	/**
+	 * Checks whether the message buffer starts and ends with the expected 6 byte-identifiers.
+	 *
+	 * @method core.protocol.message.ReadableMessage~_isProtocolMessage
+	 *
+	 * @returns {boolean} `True` if protocol message, `false` if not.
+	 */
 	private _isProtocolMessage ():boolean {
 		var msgBegin = MessageByteCheatsheet.messageBegin;
 		var msgEnd = MessageByteCheatsheet.messageEnd;
--- src/core/protocol/messages/interfaces/ReadableMessageInterface.ts
+++ src/core/protocol/messages/interfaces/ReadableMessageInterface.ts
@@ -4,20 +4,61 @@ import IdInterface = require('../../../topology/interfaces/IdInterface');
 import ContactNodeInterface = require('../../../topology/interfaces/ContactNodeInterface');
 
 /**
+ * The ReadableMessageInterface is an important part of the protocol core. Every message on the network is formatted after
+ * a specific pattern. A ReadableMessage must deconstruct a raw buffer into its parts.
+ *
+ * Roughly, a protocol message is structured into the following parts:
+ * - 6 Bytes for indicating it is a protocol message
+ * - 20 Bytes for the ID of the intended receiver
+ * - 20 Bytes for the ID of the sender
+ * - The address block, constituted by lists of IP/PORT combinations, each beginning with an indicator byte which tells if:
+ *     * IPv4 (followed by 6 reserved bytes for the address)
+ *     * IPv6 (foolowed by 18 reserved bytes for the address)
+ * - One indicator byte for the termination of the address block
+ * - Two bytes for the type of protocol message (e.g. PING)
+ * - The payload
+ * - 6 Bytes for indicating the end of the protocol message
+ *
+ *
  * @interface
  * @class core.protocol.messages.ReadableMessageInterface
  */
 interface ReadableMessageInterface {
 
+	/**
+	 * Sets payload buffer and message buffer to `null`.
+	 */
 	discard ():void;
 
-	getReceiverId ():IdInterface;
-
-	getSender ():ContactNodeInterface;
-
+	/**
+	 * Returns the extracted message type (e.g. PING, PONG etc.)
+	 *
+	 * @returns {string}
+	 */
 	getMessageType ():string;
 
+	/**
+	 * Returns the slice of the message buffer constituting the payload.
+	 * ATTENTION: Payload and message buffer reference the same memory, so altering it in one place will change it
+	 * in the other one as well.
+	 *
+	 * @returns {Buffer}
+	 */
 	getPayload ():Buffer;
+
+	/**
+	 * Returns the extracted ID of receiver.
+	 *
+	 * @returns {core.topology.IdInterface}
+	 */
+	getReceiverId ():IdInterface;
+
+	/**
+	 * Returns the extracte Contact Node-
+	 *
+	 * @returns {core.topology.ContactNodeInterface}
+	 */
+	getSender ():ContactNodeInterface;
 
 }
 
