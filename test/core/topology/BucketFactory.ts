7f9367ae21df4f9fc06ed7a16445e38cef65b669
Jörn Röder <kontakt@joernroeder.de>
2014-05-05 20:31:09 +0200

    updated BucketFactory

--- test/core/topology/BucketFactory.ts
+++ test/core/topology/BucketFactory.ts
@@ -8,6 +8,7 @@ import testUtils = require('../../utils/testUtils');
 
 import BucketFactory = require('../../../src/core/topology/BucketFactory');
 import Bucket = require('../../../src/core/topology/Bucket');
+import ContactNodeFactory = require('../../../src/core/topology/ContactNodeFactory');
 import ObjectConfig = require('../../../src/core/config/ObjectConfig');
 import BucketStore = require('../../../src/core/topology/BucketStore');
 
@@ -15,11 +16,13 @@ describe('CORE --> TOPOLOGY --> BucketFactory', function () {
 	var sandbox:SinonSandbox;
 	var configStub:any;
 	var bucketStoreStub:any;
+	var contactNodeFactoryStub:any;
 
 	beforeEach(function () {
 		sandbox = sinon.sandbox.create();
 		configStub = testUtils.stubPublicApi(sandbox, ObjectConfig);
 		bucketStoreStub = testUtils.stubPublicApi(sandbox, BucketStore);
+		contactNodeFactoryStub = testUtils.stubPublicApi(sandbox, ContactNodeFactory);
 	});
 
 	afterEach(function () {
@@ -27,7 +30,7 @@ describe('CORE --> TOPOLOGY --> BucketFactory', function () {
 	});
 
 	it ('should correctly create Buckets', function () {
-		var bucket = (new BucketFactory()).create(configStub, 'bucket', bucketStoreStub);
+		var bucket = (new BucketFactory()).create(configStub, 1, 20, bucketStoreStub, contactNodeFactoryStub);
 		bucket.should.be.an.instanceof(Bucket);
 	});
 
