aff82fbe09b6dd1f7846af10dc6e6a84bd712887
Jörn Röder <kontakt@joernroeder.de>
2014-05-07 00:13:10 +0200

    added plato (JavaScript Source Analysis), grunt-task and initial report

---
+++ build/js-source-analysis-report/files/core_topology_BucketFactory_js/report.history.js
@@ -0,0 +1,1 @@
+__history = [{"date":"Tue, 06 May 2014 22:22:15 GMT","sloc":18,"lloc":8,"functions":3,"deliveredBugs":0.06149244391890265,"maintainability":89.09146505404163,"lintErrors":0,"difficulty":6.25},{"date":"Tue, 06 May 2014 22:38:21 GMT","sloc":18,"lloc":8,"functions":3,"deliveredBugs":0.06149244391890265,"maintainability":89.09146505404163,"lintErrors":0,"difficulty":6.25}]
