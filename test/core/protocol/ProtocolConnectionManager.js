c7bed9d266ad7943487510a608f9b8d20b234073
Jonathan Pirnay <mail@johnnycrab.com>
2014-05-08 00:10:18 +0200

    tests. tests everywhere.

--- test/core/protocol/ProtocolConnectionManager.js
+++ test/core/protocol/ProtocolConnectionManager.js
@@ -47,6 +47,8 @@ describe('CORE --> PROTOCOL --> NET --> ProtocolConnectionManager @current', fun
 
     var tcpSocketFactory = new TCPSocketFactory();
 
+    var currentRemoteSocket;
+
     before(function (done) {
         sandbox = sinon.sandbox.create();
 
@@ -92,6 +94,12 @@ describe('CORE --> PROTOCOL --> NET --> ProtocolConnectionManager @current', fun
     // ------- MESSAGES ----
     var createWorkingMessageA = function () {
         var begin = new Buffer([0x50, 0x52, 0x44, 0x42, 0x47, 0x4e]), end = new Buffer([0x50, 0x52, 0x44, 0x45, 0x4e, 0x44]), receiverId = new Buffer([0xf3, 0xec, 0x6b, 0x95, 0x29, 0x92, 0xbb, 0x07, 0xf3, 0x48, 0x62, 0xa4, 0x11, 0xbb, 0x1f, 0x83, 0x3f, 0x63, 0x62, 0x88]), senderId = new Buffer([0xfe, 0x36, 0x26, 0xca, 0xca, 0x6c, 0x84, 0xfa, 0x4e, 0x5d, 0x32, 0x3b, 0x6a, 0x26, 0xb8, 0x97, 0x58, 0x2c, 0x57, 0xf9]), ipv4Address = new Buffer([0x04, 127, 0, 0, 1, 0x17, 0x71]), addressEnd = new Buffer([0x05]), messageType = new Buffer([0x50, 0x49]), payload = new Buffer('foobar', 'utf8'), list = [begin, receiverId, senderId, ipv4Address, addressEnd, messageType, payload, end];
+
+        return Buffer.concat(list);
+    };
+
+    var createWorkingMessageB = function () {
+        var begin = new Buffer([0x50, 0x52, 0x44, 0x42, 0x47, 0x4e]), end = new Buffer([0x50, 0x52, 0x44, 0x45, 0x4e, 0x44]), receiverId = new Buffer([0xf3, 0xec, 0x6b, 0x95, 0x29, 0x92, 0xbb, 0x07, 0xf3, 0x48, 0x62, 0xa4, 0x11, 0xbb, 0x1f, 0x83, 0x3f, 0x63, 0x62, 0x88]), senderId = new Buffer([0x1e, 0x36, 0x26, 0xca, 0xca, 0x6c, 0x84, 0xfa, 0x4e, 0x5d, 0x32, 0x3b, 0x6a, 0x26, 0xb8, 0x97, 0x58, 0x2c, 0x57, 0xf9]), ipv4Address = new Buffer([0x04, 127, 0, 0, 1, 0x17, 0x71]), addressEnd = new Buffer([0x05]), messageType = new Buffer([0x50, 0x49]), payload = new Buffer('foobar', 'utf8'), list = [begin, receiverId, senderId, ipv4Address, addressEnd, messageType, payload, end];
 
         return Buffer.concat(list);
     };
@@ -190,6 +198,54 @@ describe('CORE --> PROTOCOL --> NET --> ProtocolConnectionManager @current', fun
                 }
             }
         });
+    });
+
+    it('should swap out the outgoing connection with the inbound connection', function (done) {
+        var sock = net.createConnection(protoPort, 'localhost');
+        tcpSocketHandler.once('connected', function () {
+            manager.on('terminatedConnection', function () {
+                throw new Error('Should definitely not do that, nope nope.');
+            });
+            manager.on('confirmedSocket', function (identifier, socket) {
+                manager.removeAllListeners('terminatedConnection');
+                if (manager.getConfirmedSocketList()[identifier].direction === 'incoming') {
+                    if (Object.keys(manager.getIncomingPendingSocketList()).length === 0) {
+                        if (Object.keys(manager.getOutgoingPendingSocketList()).length === 0) {
+                            done();
+                        }
+                    }
+                }
+            });
+            sock.write(createWorkingMessageA());
+        });
+    });
+
+    it('should prefer the incoming connection and close the outgoing pending at once when a connection has been established', function (done) {
+        var ident = '1e3626caca6c84fa4e5d323b6a26b897582c57f9';
+        var id = new Id(Id.byteBufferByHexString(ident, 20), 160);
+        var goodContactNode = nodeFactory.create(id, [addressFactory.create('14.213.160.0', 1111), addressFactory.create('127.0.0.1', remotePort)]);
+
+        manager.obtainConnectionTo(goodContactNode, function (err, socket) {
+            // check that is is inbound
+            if (manager.getOutgoingPendingSocketList()[ident].closeAtOnce === true) {
+                if (manager.getConfirmedSocketList()[ident].direction === 'incoming') {
+                    setTimeout(function () {
+                        if (Object.keys(manager.getOutgoingPendingSocketList()).length === 0) {
+                            manager.removeAllListeners('terminatedConnection');
+                            done();
+                        }
+                    }, 500);
+                }
+            }
+        });
+
+        tcpSocketHandler.once('connected', function () {
+            manager.on('terminatedConnection', function () {
+                throw new Error('Should definitely not do that, nope nope.');
+            });
+            currentRemoteSocket.write(createWorkingMessageB());
+        });
+        currentRemoteSocket = net.createConnection(protoPort, 'localhost');
     });
 });
 //# sourceMappingURL=ProtocolConnectionManager.js.map
