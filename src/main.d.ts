339dfea510e4e1da1fb3d9263281df0618b0e1c4
Jörn Röder <kontakt@joernroeder.de>
2014-05-07 13:12:00 +0200

    added "fs-extra" npm package

--- src/main.d.ts
+++ src/main.d.ts
@@ -1,2 +1,3 @@
 /// <reference path='../ts-definitions/node/node.d.ts' />
 /// <reference path='../ts-definitions/node-webkit/node-webkit.d.ts' />
+/// <reference path='../ts-definitions/fs-extra/fs-extra.d.ts' />
