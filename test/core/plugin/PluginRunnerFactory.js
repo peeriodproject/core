6dc0341fb37d8f682dd28cc9dc43eb2736781646
Jörn Röder <kontakt@joernroeder.de>
2014-05-13 20:31:44 +0200

    cleanup

--- test/core/plugin/PluginRunnerFactory.js
+++ test/core/plugin/PluginRunnerFactory.js
@@ -5,7 +5,7 @@ var testUtils = require('../../utils/testUtils');
 var PluginRunnerFactory = require('../../../src/core/plugin/PluginRunnerFactory');
 var PluginRunner = require('../../../src/core/plugin/PluginRunner');
 
-describe('CORE --> PLUGIN --> PluginRunnerFactory @joern', function () {
+describe('CORE --> PLUGIN --> PluginRunnerFactory', function () {
     it('should correctly create plugin runners', function () {
         var pluginRunner = (new PluginRunnerFactory()).create('identifier', testUtils.getFixturePath('/plugin/pluginRunner/emptyFile.js'));
         pluginRunner.should.be.an.instanceof(PluginRunner);
