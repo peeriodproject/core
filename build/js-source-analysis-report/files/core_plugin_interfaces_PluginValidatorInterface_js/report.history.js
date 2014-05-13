4ef1de2681c4a7f64ba6c6687a24c0168b7109e0
Jörn Röder <kontakt@joernroeder.de>
2014-05-13 18:18:48 +0200

    updated js analysis report

--- build/js-source-analysis-report/files/core_plugin_interfaces_PluginValidatorInterface_js/report.history.js
+++ build/js-source-analysis-report/files/core_plugin_interfaces_PluginValidatorInterface_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Tue, 13 May 2014 12:52:34 GMT","sloc":1,"lloc":0,"functions":0,"deliveredBugs":0,"maintainability":100,"lintErrors":0,"difficulty":0}]
+__history = [{"date":"Tue, 13 May 2014 12:52:34 GMT","sloc":1,"lloc":0,"functions":0,"deliveredBugs":0,"maintainability":100,"lintErrors":0,"difficulty":0},{"date":"Tue, 13 May 2014 16:18:24 GMT","sloc":1,"lloc":0,"functions":0,"deliveredBugs":0,"maintainability":100,"lintErrors":0,"difficulty":0}]
