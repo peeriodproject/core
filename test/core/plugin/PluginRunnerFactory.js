b031abb32c3353772c2442e2369ac2320ea8b146
Jörn Röder <kontakt@joernroeder.de>
2014-05-13 13:52:56 +0200

    updated PluginRunnerFactory tests

--- test/core/plugin/PluginRunnerFactory.js
+++ test/core/plugin/PluginRunnerFactory.js
@@ -1,12 +1,13 @@
 /// <reference path='../../test.d.ts' />
 require('should');
+var testUtils = require('../../utils/testUtils');
 
 var PluginRunnerFactory = require('../../../src/core/plugin/PluginRunnerFactory');
 var PluginRunner = require('../../../src/core/plugin/PluginRunner');
 
-describe('CORE --> PLUGIN --> PluginRunnerFactory', function () {
+describe('CORE --> PLUGIN --> PluginRunnerFactory @joern', function () {
     it('should correctly create plugin runners', function () {
-        var pluginRunner = (new PluginRunnerFactory()).create('identifier', '/path/to/runnable/file.js');
+        var pluginRunner = (new PluginRunnerFactory()).create('identifier', testUtils.getFixturePath('/plugin/pluginRunner/emptyFile.js'));
         pluginRunner.should.be.an.instanceof(PluginRunner);
     });
 });
--- test/core/plugin/PluginRunnerFactory.ts
+++ test/core/plugin/PluginRunnerFactory.ts
@@ -1,14 +1,15 @@
 /// <reference path='../../test.d.ts' />
 
 require('should');
+import testUtils = require('../../utils/testUtils');
 
 import PluginRunnerFactory = require('../../../src/core/plugin/PluginRunnerFactory');
 import PluginRunner = require('../../../src/core/plugin/PluginRunner');
 
-describe('CORE --> PLUGIN --> PluginRunnerFactory', function () {
+describe('CORE --> PLUGIN --> PluginRunnerFactory @joern', function () {
 
 	it ('should correctly create plugin runners', function () {
-		var pluginRunner = (new PluginRunnerFactory()).create('identifier', '/path/to/runnable/file.js');
+		var pluginRunner = (new PluginRunnerFactory()).create('identifier', testUtils.getFixturePath('/plugin/pluginRunner/emptyFile.js'));
 		pluginRunner.should.be.an.instanceof(PluginRunner);
 	});
 
