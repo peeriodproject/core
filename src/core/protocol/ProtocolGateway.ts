ff13b4db7b7ecf4c092da5844f71b5cd4edcc4dd
Jonathan Pirnay <mail@johnnycrab.com>
2014-05-15 11:43:49 +0200

    added proxy manager to protocol gateway

--- src/core/protocol/ProtocolGateway.ts
+++ src/core/protocol/ProtocolGateway.ts
@@ -1,9 +1,12 @@
 import ProtocolGatewayInterface = require('./interfaces/ProtocolGatewayInterface');
 import ProtocolConnectionManagerInterface = require('./net/interfaces/ProtocolConnectionManagerInterface');
 import ProtocolConnectionManager = require('./net/ProtocolConnectionManager');
+import ProxyManagerInterface = require('./proxy/interfaces/ProxyManagerInterface');
+import ProxyManager = require('./proxy/ProxyManager');
 import TCPSocketHandlerInterface = require('../net/tcp/interfaces/TCPSocketHandlerInterface');
 import MyNodeInterface = require('../topology/interfaces/MyNodeInterface');
 import ConfigInterface = require('../config/interfaces/ConfigInterface');
+import RoutingTableInterface = require('../topology/interfaces/RoutingTableInterface');
 
 class ProtocolGateway implements ProtocolGatewayInterface {
 
@@ -12,15 +15,20 @@ class ProtocolGateway implements ProtocolGatewayInterface {
 	private _tcpSocketHandler:TCPSocketHandlerInterface = null;
 	private _protocolConfig:ConfigInterface = null;
 	private _protocolConnectionManager:ProtocolConnectionManagerInterface = null;
+	private _proxyManager:ProxyManagerInterface = null;
+	private _routingTable:RoutingTableInterface = null;
 
-	constructor (protocolConfig:ConfigInterface, myNode:MyNodeInterface, tcpSocketHandler:TCPSocketHandlerInterface) {
+	constructor (protocolConfig:ConfigInterface, myNode:MyNodeInterface, tcpSocketHandler:TCPSocketHandlerInterface, routingTable:RoutingTableInterface) {
 		this._protocolConfig = protocolConfig;
 		this._myNode = myNode;
 		this._tcpSocketHandler = tcpSocketHandler;
+		this._routingTable = routingTable;
 
 		// build up the ProtocolConnectionManager
 		this._protocolConnectionManager = new ProtocolConnectionManager(this._protocolConfig, this._myNode, this._tcpSocketHandler);
 
+		// build up zhr ProxyManager
+		this._proxyManager = new ProxyManager(this._protocolConfig, this._protocolConnectionManager, this._routingTable);
 
 	}
 
