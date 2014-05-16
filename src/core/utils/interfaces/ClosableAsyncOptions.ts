f30893dc4573587082b5eb76a6b9ac9410cccd15
Jörn Röder <kontakt@joernroeder.de>
2014-05-16 18:45:20 +0200

    added ClosableOptions interface

--- src/core/utils/interfaces/ClosableAsyncOptions.ts
+++ src/core/utils/interfaces/ClosableAsyncOptions.ts
@@ -1,9 +1,10 @@
+import ClosableOptions = require('./ClosableOptions');
+
 /**
  * @interface
  * @class core.utils.ClosableAsyncOptions
  */
-interface ClosableAsyncOptions {
-	closeOnProcessExit?: boolean;
+interface ClosableAsyncOptions extends ClosableOptions {
 	onCloseCallback?: (err:Error) => any;
 	onOpenCallback?: (err:Error) => any;
 }
