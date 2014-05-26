9e593f491251a58d860f1dc17dfd455c0224c553
Jörn Röder <kontakt@joernroeder.de>
2014-05-26 23:38:09 +0200

    updated document base plugin

--- src/plugins/textDocumentPlugin/lib/index.js
+++ src/plugins/textDocumentPlugin/lib/index.js
@@ -12,15 +12,47 @@
 
     /**
     * Returns the mapping used in the elasticsearch index to store the plugin data
+    * The mapping doesn't include the document root!
     *
     * @see http://www.elasticsearch.org/guide/en/elasticsearch/reference/current/indices-put-mapping.html
     */
     function getMapping() {
         var mapping = {
-            textdocument: {
-                properties: {
-                    file_attachment: {
-                        type: 'attachment'
+            _source: {
+                excludes: ['file']
+            },
+            properties: {
+                file: {
+                    type: 'attachment',
+                    indexed_chars: -1,
+                    detect_anguage: true,
+                    fields: {
+                        file: {
+                            store: 'yes',
+                            term_vector: 'with_positions_offsets'
+                        },
+                        author: {
+                            store: 'yes'
+                        },
+                        title: {
+                            store: 'yes'
+                        },
+                        date: {
+                            store: 'yes'
+                        },
+                        keywords: {
+                            store: 'yes',
+                            analyzer: 'keyword'
+                        },
+                        content_type: {
+                            store: 'yes'
+                        },
+                        content_length: {
+                            store: 'yes'
+                        },
+                        language: {
+                            store: 'yes'
+                        }
                     }
                 }
             }
