2decee7207332b544a5695e39d4fd784e31de92d
Jörn Röder <kontakt@joernroeder.de>
2014-05-27 02:10:27 +0200

    updated js analysis report

---
+++ build/js-source-analysis-report/files/core_protocol_findClosestNodes_FindClosestNodesCycle_js/report.history.js
@@ -0,0 +1,1 @@
+__history = [{"date":"Tue, 27 May 2014 00:05:38 GMT","sloc":153,"lloc":106,"functions":12,"deliveredBugs":1.4277163404252535,"maintainability":63.53604545475957,"lintErrors":0,"difficulty":50.60416666666667}]
