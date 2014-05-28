b8e5c1389d1157fc55ec9c61fd64dc6485c037f5
Jonathan Pirnay <mail@johnnycrab.com>
2014-05-28 21:20:46 +0200

    documentation / code styles

--- src/core/protocol/networkMaintenance/nodeDiscovery/nodeSeeker/NodeSeeker.js
+++ src/core/protocol/networkMaintenance/nodeDiscovery/nodeSeeker/NodeSeeker.js
@@ -2,29 +2,63 @@
 var Id = require('../../../../topology/Id');
 
 /**
+* The NodeSeeker class is a helper class from which other seeker classes can be extended from, offering a JSON-to-node
+* conversion and the construction of addresses/nodes via provided factories.
+*
 * @class core.protocol.nodeDiscovery.NodeSeeker
 */
 var NodeSeeker = (function () {
     function NodeSeeker() {
-        this._contactNodeFactory = null;
+        /**
+        * A contact node address factory.
+        *
+        * @member {core.topology.ContactNodeAddressFactoryInterface} core.protocol.nodeDiscovery.NodeSeeker~_addressFactory
+        */
         this._addressFactory = null;
+        /**
+        * A contact node factory.
+        *
+        * @member {core.topology.ContactNodeFactoryInterface} core.protocol.nodeDiscovery.NodeSeeker~_nodeFactory
+        */
+        this._contactNodeFactory = null;
     }
-    NodeSeeker.prototype.setNodeFactory = function (factory) {
-        this._contactNodeFactory = factory;
-    };
-
-    NodeSeeker.prototype.setAddressFactory = function (factory) {
-        this._addressFactory = factory;
-    };
-
-    NodeSeeker.prototype.getNodeFactory = function () {
-        return this._contactNodeFactory;
-    };
-
+    /**
+    * Getter for the address factory.
+    *
+    * @method core.protocol.nodeDiscovery.NodeSeeker#getAddressFactory
+    *
+    * @returns core.topology.ContactNodeAddressFactoryInterface
+    */
     NodeSeeker.prototype.getAddressFactory = function () {
         return this._addressFactory;
     };
 
+    /**
+    * Getter for the node factory.
+    *
+    * @method core.protocol.nodeDiscovery.NodeSeeker#getNodeFactory
+    *
+    * @returns core.topology.ContactNodeFactoryInterface
+    */
+    NodeSeeker.prototype.getNodeFactory = function () {
+        return this._contactNodeFactory;
+    };
+
+    /**
+    * Tries to create a contact node from a JSON object.
+    * The JSON object must look like the following:
+    * {
+    *   'id' : a hex string representation of the node's id
+    *   'addresses': an array of {ip:string, port:number}-objects
+    * }
+    *
+    * Throws errors on problems.
+    *
+    * @method core.protocol.nodeDiscovery.NodeSeeker#nodeFromJSON
+    *
+    * @param {Object} obj A JSON object
+    * @returns {core.topology.ContactNodeInterface} The resulting contact node (if successful)
+    */
     NodeSeeker.prototype.nodeFromJSON = function (obj) {
         var id = new Id(Id.byteBufferByHexString(obj.id, 20), 160);
         var addresses = [];
@@ -38,6 +72,28 @@ var NodeSeeker = (function () {
         }
 
         return this.getNodeFactory().create(id, addresses);
+    };
+
+    /**
+    * Setter for the address factory.
+    *
+    * @method core.protocol.nodeDiscovery.NodeSeeker#setAddressFactory
+    *
+    * @param {core.topology.ContactNodeAddressFactoryInterface} factory
+    */
+    NodeSeeker.prototype.setAddressFactory = function (factory) {
+        this._addressFactory = factory;
+    };
+
+    /**
+    * Setter for the node factory.
+    *
+    * @method core.protocol.nodeDiscovery.NodeSeeker#setNodeFactory
+    *
+    * @param {core.topology.ContactNodeFactoryInterface} factory
+    */
+    NodeSeeker.prototype.setNodeFactory = function (factory) {
+        this._contactNodeFactory = factory;
     };
     return NodeSeeker;
 })();
