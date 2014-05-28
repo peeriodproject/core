bb912af4a9c465ae62c795ab69f2cde69a09ddcf
Jonathan Pirnay <mail@johnnycrab.com>
2014-05-28 21:33:21 +0200

    documentation / code styles

--- src/core/protocol/networkMaintenance/nodeDiscovery/nodeSeeker/RoutingTableNodeSeeker.js
+++ src/core/protocol/networkMaintenance/nodeDiscovery/nodeSeeker/RoutingTableNodeSeeker.js
@@ -1,7 +1,21 @@
+/**
+* A node seeker which returns a random node from a routing table.
+* If none is found, `null` is returned.
+*
+* @class core.protocol.nodeDiscovery.RoutingTableNodeSeeker
+* @implements core.protocol.nodeDiscovery.NodeSeekerInterface
+*
+* @param {core.topology.RoutingTableInterface} A routing table.
+*/
 var RoutingTableNodeSeeker = (function () {
     function RoutingTableNodeSeeker(routingTable) {
+        /**
+        * A routing table.
+        *
+        * @member {core.topology.RoutingTableInterface} core.protocol.nodeDiscovery.RoutingTableNodeSeeker~_routingTable
+        */
         this._routingTable = null;
-        this._routingTable = null;
+        this._routingTable = routingTable;
     }
     RoutingTableNodeSeeker.prototype.seek = function (callback) {
         this._routingTable.getRandomContactNode(function (err, node) {
--- src/core/protocol/networkMaintenance/nodeDiscovery/nodeSeeker/RoutingTableNodeSeeker.ts
+++ src/core/protocol/networkMaintenance/nodeDiscovery/nodeSeeker/RoutingTableNodeSeeker.ts
@@ -2,13 +2,26 @@ import NodeSeekerInterface = require('./interfaces/NodeSeekerInterface');
 import ContactNodeInterface = require('../../../../topology/interfaces/ContactNodeInterface');
 import RoutingTableInterface = require('../../../../topology/interfaces/RoutingTableInterface');
 
+/**
+ * A node seeker which returns a random node from a routing table.
+ * If none is found, `null` is returned.
+ *
+ * @class core.protocol.nodeDiscovery.RoutingTableNodeSeeker
+ * @implements core.protocol.nodeDiscovery.NodeSeekerInterface
+ *
+ * @param {core.topology.RoutingTableInterface} A routing table.
+ */
 class RoutingTableNodeSeeker implements NodeSeekerInterface {
 
+	/**
+	 * A routing table.
+	 *
+	 * @member {core.topology.RoutingTableInterface} core.protocol.nodeDiscovery.RoutingTableNodeSeeker~_routingTable
+	 */
 	private _routingTable:RoutingTableInterface = null;
 
 	constructor (routingTable:RoutingTableInterface) {
-
-		this._routingTable = null;
+		this._routingTable = routingTable;
 	}
 
 	public seek (callback:(node:ContactNodeInterface) => any):void {
