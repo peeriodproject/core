04ca7b8bc9af1c9cc298548391c2b42e207a1379
Jonathan Pirnay <mail@johnnycrab.com>
2014-05-31 23:56:24 +0200

    join network. completed topology.

--- src/core/protocol/ProtocolGateway.js
+++ src/core/protocol/ProtocolGateway.js
@@ -8,10 +8,19 @@ var FindClosestNodesCycleFactory = require('./findClosestNodes/FindClosestNodesC
 var FoundClosestNodesWritableMessageFactory = require('./findClosestNodes/messages/FoundClosestNodesWritableMessageFactory');
 var FoundClosestNodesReadableMessageFactory = require('./findClosestNodes/messages/FoundClosestNodesReadableMessageFactory');
 
+var NodeSeekerManager = require('./networkMaintenance/nodeDiscovery/nodeSeeker/NodeSeekerManager');
+var NodeSeekerFactory = require('./networkMaintenance/nodeDiscovery/nodeSeeker/NodeSeekerFactory');
+
+var NodePublisherFactory = require('./networkMaintenance/nodeDiscovery/nodePublisher/NodePublisherFactory');
+
+var NetworkMaintainer = require('./networkMaintenance/NetworkMaintainer');
+
 var ProtocolGateway = (function () {
-    function ProtocolGateway(protocolConfig, topologyConfig, myNode, tcpSocketHandler, routingTable) {
+    function ProtocolGateway(appConfig, protocolConfig, topologyConfig, myNode, tcpSocketHandler, routingTable) {
+        var _this = this;
         this._myNode = null;
         this._tcpSocketHandler = null;
+        this._appConfig = null;
         this._protocolConfig = null;
         this._protocolConnectionManager = null;
         this._proxyManager = null;
@@ -19,6 +28,10 @@ var ProtocolGateway = (function () {
         this._topologyConfig = null;
         this._pingPongNodeUpdateHandler = null;
         this._findClosestNodesManager = null;
+        this._nodeSeekerManager = null;
+        this._nodePublishers = null;
+        this._networkMaintainer = null;
+        this._appConfig = appConfig;
         this._protocolConfig = protocolConfig;
         this._topologyConfig = topologyConfig;
 
@@ -41,7 +54,40 @@ var ProtocolGateway = (function () {
         var foundClosestNodesReadableMessageFactory = new FoundClosestNodesReadableMessageFactory();
 
         this._findClosestNodesManager = new FindClosestNodesManager(this._topologyConfig, this._protocolConfig, this._myNode, this._protocolConnectionManager, this._proxyManager, this._routingTable, findClosestNodesCycleFactory, foundClosestNodesWritableMessageFactory, foundClosestNodesReadableMessageFactory);
+
+        // build up the NodeSeekerManager
+        var nodeSeekerFactory = new NodeSeekerFactory(this._appConfig, this._routingTable);
+
+        this._nodeSeekerManager = new NodeSeekerManager(nodeSeekerFactory, this._protocolConnectionManager, this._proxyManager);
+
+        // build up the NodePublishers
+        var nodePublisherFactory = new NodePublisherFactory(appConfig, this._myNode);
+
+        nodePublisherFactory.createPublisherList(function (list) {
+            _this._nodePublishers = list;
+        });
+
+        // build up the NetworkMaintainer
+        this._networkMaintainer = new NetworkMaintainer(this._topologyConfig, this._protocolConfig, this._myNode, this._nodeSeekerManager, this._findClosestNodesManager, this._proxyManager);
     }
+    ProtocolGateway.prototype.start = function () {
+        var _this = this;
+        /**
+        *
+        * If it needs a proxy, kick off proxy manager only when the NetworkMaintainer has finished its entry
+        * If it doesnt need a proxy, kick off proxy manager right away
+        *
+        */
+        if (this._proxyManager.needsAdditionalProxy()) {
+            this._networkMaintainer.on('initialContactQueryCompleted', function () {
+                _this._proxyManager.kickOff();
+            });
+        } else {
+            this._proxyManager.kickOff();
+        }
+
+        this._networkMaintainer.joinNetwork();
+    };
     return ProtocolGateway;
 })();
 
