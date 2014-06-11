78ef7af7a0c3031e4abbacba7a74978460a9b427
Jörn Röder <kontakt@joernroeder.de>
2014-06-11 19:50:52 +0200

    updated js analysis report

---
+++ build/js-source-analysis-report/files/core_protocol_hydra_messages_Aes128GcmLayeredEncDecHandler_js/report.history.js
@@ -0,0 +1,1 @@
+__history = [{"date":"Wed, 11 Jun 2014 17:45:20 GMT","sloc":111,"lloc":72,"functions":11,"deliveredBugs":0.8537664670551296,"maintainability":68.36311762525918,"lintErrors":0,"difficulty":44.01724137931034}]
---
+++ build/js-source-analysis-report/files/core_ui_folder_UiFolderWatcherManagerComponent_js/report.history.js
@@ -0,0 +1,1 @@
+__history = [{"date":"Wed, 11 Jun 2014 17:45:23 GMT","sloc":216,"lloc":82,"functions":20,"deliveredBugs":1.0405193447558265,"maintainability":74.3473600630001,"lintErrors":0,"difficulty":42.58064516129032}]
