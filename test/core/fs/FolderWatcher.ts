fc221f621b0f95e8135db48b608a41e6e46921b9
Jörn Röder <kontakt@joernroeder.de>
2014-06-12 12:59:11 +0200

    cleanup

--- test/core/fs/FolderWatcher.ts
+++ test/core/fs/FolderWatcher.ts
@@ -6,7 +6,6 @@ require('should');
 import sinon = require('sinon');
 import testUtils = require('../../utils/testUtils');
 
-
 import ObjectConfig = require('../../../src/core/config/ObjectConfig');
 import FolderWatcher = require('../../../src/core/fs/FolderWatcher');
 
