044eb9c32020820e1554f1b2ae1c46294cd52334
Jonathan Pirnay <mail@johnnycrab.com>
2014-06-17 14:57:26 +0200

    wip

--- src/core/protocol/hydra/HydraCircuit.js
+++ src/core/protocol/hydra/HydraCircuit.js
@@ -8,8 +8,24 @@ var events = require('events');
 
 var HydraCircuit = (function (_super) {
     __extends(HydraCircuit, _super);
-    function HydraCircuit() {
-        _super.apply(this, arguments);
+    function HydraCircuit(hydraConfig, numOfRelayNodes, nodePicker, messageCenter, connectionManager, layeredEncDecFactory) {
+        _super.call(this);
+        this._numOfRelayNodes = 0;
+        this._additiveSharingNodeAmount = 0;
+        this._nodePicker = null;
+        this._messageCenter = null;
+        this._connectionManager = null;
+        this._extensionReactionTimeBaseInMs = 0;
+        this._extensionReactionTimeFactor = 0;
+        this._layeredEncDecFactory = null;
+        this._numOfRelayNodes = numOfRelayNodes;
+        this._additiveSharingNodeAmount = hydraConfig.get('hydra.additiveSharingNodeAmount');
+        this._nodePicker = nodePicker;
+        this._messageCenter = messageCenter;
+        this._connectionManager = connectionManager;
+        this._extensionReactionTimeFactor = hydraConfig.get('hydra.circuit.extensionReactionTimeFactor');
+        this._extensionReactionTimeBaseInMs = hydraConfig.get('hydra.circuit.extensionReactionTimeBaseInSeconds') * 1000;
+        this._layeredEncDecFactory = layeredEncDecFactory;
     }
     return HydraCircuit;
 })(events.EventEmitter);
