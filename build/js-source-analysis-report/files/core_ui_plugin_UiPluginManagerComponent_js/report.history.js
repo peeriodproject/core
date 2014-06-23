553cb33598075dff00ebb64f077dc0768b135144
Jörn Röder <kontakt@joernroeder.de>
2014-06-23 08:32:46 +0200

    updated js analysis report

---
+++ build/js-source-analysis-report/files/core_ui_plugin_UiPluginManagerComponent_js/report.history.js
@@ -0,0 +1,1 @@
+__history = [{"date":"Mon, 23 Jun 2014 06:29:04 GMT","sloc":125,"lloc":62,"functions":18,"deliveredBugs":0.684,"maintainability":76.52463940220962,"lintErrors":1,"difficulty":28.01020408163265}]
