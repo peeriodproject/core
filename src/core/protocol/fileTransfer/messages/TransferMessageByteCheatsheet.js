26896f6e30b112b776a88361781b741bc8256583
Jonathan Pirnay <mail@johnnycrab.com>
2014-06-25 16:50:58 +0200

    Readable / writable general FileTransferMessage

---
+++ src/core/protocol/fileTransfer/messages/TransferMessageByteCheatsheet.js
@@ -0,0 +1,8 @@
+var sheet = {
+    messageTypes: {
+        "TRANSFER_QUERY": 0x01
+    }
+};
+
+module.exports = sheet;
+//# sourceMappingURL=TransferMessageByteCheatsheet.js.map
---
+++ test/core/protocol/fileTransfer/WritableFileTransferMessageFactory.js
@@ -0,0 +1,31 @@
+/// <reference path='../../../test.d.ts' />
+require('should');
+
+var WritableFileTransferMessageFactory = require('../../../../src/core/protocol/fileTransfer/messages/WritableFileTransferMessageFactory');
+
+describe('CORE --> PROTOCOL --> FILE TRANSFER --> WritableFileTransferMessageFactory', function () {
+    var factory = new WritableFileTransferMessageFactory();
+
+    it('should correctly format the message', function () {
+        var expected = Buffer.concat([new Buffer('cafebabecafebabecafebabecafebabe', 'hex'), new Buffer([0x01]), new Buffer('foobar')]);
+
+        var actual = factory.constructMessage('cafebabecafebabecafebabecafebabe', 'TRANSFER_QUERY', new Buffer('foobar'));
+
+        actual.length.should.equal(expected.length);
+
+        for (var i = 0; i < actual.length; i++) {
+            actual[i].should.equal(expected[i]);
+        }
+    });
+
+    it('should throw the errors', function () {
+        (function () {
+            factory.constructMessage('cafebabe', 'TRANSFER_QUERY', new Buffer('foobar'));
+        }).should.throw();
+
+        (function () {
+            factory.constructMessage('cafebabecafebabecafebabecafebabe', 'FOOBAR', new Buffer('foobar'));
+        }).should.throw();
+    });
+});
+//# sourceMappingURL=WritableFileTransferMessageFactory.js.map
