7070f46af8da9c5e37402f14125d95555e3de658
Jonathan Pirnay <mail@johnnycrab.com>
2014-07-04 15:18:04 +0200

    more documentation

--- src/core/protocol/fileTransfer/Middleware.js
+++ src/core/protocol/fileTransfer/Middleware.js
@@ -1,11 +1,54 @@
+/**
+* MiddlewareInterface implementation.
+*
+* @class core.protocol.fileTransfer.Middleware
+* @implements core.protocol.fileTransfer.MiddlewareInterface
+*
+* @param {core.protocol.hydra.CellManagerInterface} A working hydra cell manager.
+* @param {core.protocol.net.ProtocolConnectionManagerInterface} A working protocol connection manager.
+* @param {core.protocol.hydra.HydraMessageCenterInterface} A working hydra message center.
+* @param {core.protocol.fileTransfer.WritableFileTransferMessageFactoryInterface} Factory for writable FILE_TRANSFER messages
+*/
 var Middleware = (function () {
     function Middleware(cellManager, protocolConnectionManager, hydraMessageCenter, writableFileTransferMessageFactory) {
+        /**
+        * Stores the hydra cell manager instance.
+        *
+        * @member {core.protocol.hydra.CellManagerInterface} core.protocol.fileTransfer.Middleware~_cellManager
+        */
         this._cellManager = null;
-        this._protocolConnectionManager = null;
+        /**
+        * Stores the hydra message center.
+        *
+        * @member {core.protocol.hydra.HydraMessageCenterInterface} core.protocol.fileTransfer.Middleware~_hydraMessageCenter
+        */
         this._hydraMessageCenter = null;
-        this._writableFileTransferMessageFactory = null;
-        this._outgoingSockets = {};
+        /**
+        * Stores references to incoming middleware sockets assigned to the identifier of the circuit through which the messages should
+        * be piped through.
+        *
+        * @member {Object} core.protocol.fileTransfer.Middleware~_incomingSockets
+        */
         this._incomingSockets = {};
+        /**
+        * Stores references to outgoing middleware sockets assigned to the concatenation of the fed node's attributes + the circuit
+        * through which the EXTERNAL_FEED message came.
+        *
+        * @member {Object} core.protocol.fileTransfer.Middleware~_outgoingSockets
+        */
+        this._outgoingSockets = {};
+        /**
+        * Stores the protocol connection manager.
+        *
+        * @member {core.protocol.net.ProtcolConnectionManagerInterface} core.protocol.fileTransfer.Middleware~_protocolConnectionManager
+        */
+        this._protocolConnectionManager = null;
+        /**
+        * Stores the factory for writable FILE_TRANSFER messages
+        *
+        * @member {core.protocol.fileTransfer.WritableFileTransferMessageFactoryInterface} core.protocol.fileTransfer.Middleware~_writableFileTransferMessageFactory
+        */
+        this._writableFileTransferMessageFactory = null;
         this._cellManager = cellManager;
         this._protocolConnectionManager = protocolConnectionManager;
         this._hydraMessageCenter = hydraMessageCenter;
@@ -13,6 +56,20 @@ var Middleware = (function () {
 
         this._setupListeners();
     }
+    Middleware.prototype.addIncomingSocket = function (circuitId, socketIdentifier) {
+        var existing = this._incomingSockets[circuitId];
+
+        if (existing) {
+            existing.push(socketIdentifier);
+        } else {
+            this._incomingSockets[circuitId] = [socketIdentifier];
+        }
+    };
+
+    Middleware.prototype.closeSocketByIdentifier = function (socketIdentifier) {
+        this._protocolConnectionManager.closeHydraSocket(socketIdentifier);
+    };
+
     Middleware.prototype.feedNode = function (feedingNodes, associatedCircuitId, payloadToFeed) {
         var fed = false;
 
@@ -32,20 +89,55 @@ var Middleware = (function () {
         }
     };
 
-    Middleware.prototype.addIncomingSocket = function (circuitId, socketIdentifier) {
-        var existing = this._incomingSockets[circuitId];
+    /**
+    * Self explanatory method. Used for assigning outgoing socket identifiers to nodes / circuits.
+    *
+    * @method core.protocol.fileTransfer.Middleware~_constructOutgoingKey
+    *
+    * @param {core.protocol.hydra.HydraNode} node The potential node to feed.
+    * @param {string} circuitId The identifier of the circuit through which the EXTERNAL_FEED message came through
+    * @returns {string} The concatenation used as identifier to assign a socket to.
+    */
+    Middleware.prototype._constructOutgoingKey = function (node, circuitId) {
+        return circuitId + '_' + node.ip + '_' + node.port + '_' + node.feedingIdentifier;
+    };
 
-        if (existing) {
-            existing.push(socketIdentifier);
-        } else {
-            this._incomingSockets[circuitId] = [socketIdentifier];
+    /**
+    * Wraps a payload ina GOT_FED message, which again is wrapped in a FILE_TRANSFER message, and pipes it through
+    * the TCP hydra socket stored under the given identifier (in the protocol connection manager).
+    *
+    * @method core.protocol.fileTransfer.Middleware~_feedSocket
+    *
+    * @param {string} socketIdentifier The TCP hydra socket to use.
+    * @param {string} feedingIdentifier The feedingIdentifier of the node to feed. This is used as transferId for the FILE_TRANSFER message.
+    * @param {Buffer} payloadToFeed The complete buffer of the message to feed.
+    */
+    Middleware.prototype._feedSocket = function (socketIdentifier, feedingIdentifier, payloadToFeed) {
+        var bufferToSend = null;
+
+        try  {
+            bufferToSend = this._hydraMessageCenter.wrapFileTransferMessage(this._writableFileTransferMessageFactory.constructMessage(feedingIdentifier, 'GOT_FED', payloadToFeed));
+        } catch (e) {
+        }
+
+        if (bufferToSend) {
+            this._protocolConnectionManager.hydraWriteMessageTo(socketIdentifier, bufferToSend);
         }
     };
 
-    Middleware.prototype.closeSocketByIdentifier = function (socketIdentifier) {
-        this._protocolConnectionManager.closeHydraSocket(socketIdentifier);
-    };
-
+    /**
+    * Tries to open a TCP socket to one of the nodes of the provided list. This is done in the fashion described in
+    * {@link core.protocol.fileTransfer.MiddlewareInterface}.
+    * As soon as a connection has been established, the payload is fed to it.
+    *
+    * @method core.protocol.fileTransfer.Middleware~_obtainConnectionAndFeed
+    *
+    * @param {core.protocol.hydra.HydraNodeList} feedingNodes List of potential nodes to feed. The payload is of course, however, only fed to ONE node.
+    * @param {string} associatedCircuitId The identifier of the circuit which the originating EXTERNAL_FEED message came through
+    * @param {Buffer} payloadToFeed The payload to feed.
+    * @param {Array<number>} usedIndices Optional, and only used internally if a follow-up call to this method must be performed. Indicates which nodes in the
+    * list have already been probed.
+    */
     Middleware.prototype._obtainConnectionAndFeed = function (feedingNodes, associatedCircuitId, payloadToFeed, usedIndices) {
         var _this = this;
         if (typeof usedIndices === "undefined") { usedIndices = []; }
@@ -73,23 +165,11 @@ var Middleware = (function () {
         }
     };
 
-    Middleware.prototype._feedSocket = function (socketIdentifier, feedingIdentifier, payloadToFeed) {
-        var bufferToSend = null;
-
-        try  {
-            bufferToSend = this._hydraMessageCenter.wrapFileTransferMessage(this._writableFileTransferMessageFactory.constructMessage(feedingIdentifier, 'GOT_FED', payloadToFeed));
-        } catch (e) {
-        }
-
-        if (bufferToSend) {
-            this._protocolConnectionManager.hydraWriteMessageTo(socketIdentifier, bufferToSend);
-        }
-    };
-
-    Middleware.prototype._constructOutgoingKey = function (node, circuitId) {
-        return circuitId + '_' + node.ip + '_' + node.port + '_' + node.feedingIdentifier;
-    };
-
+    /**
+    * Sets up the appropriate listeners on the cell manager and the protocol connection manager. Called in constructor.
+    *
+    * @method core.protocol.fileTransfer.Middleware~_setupListeners
+    */
     Middleware.prototype._setupListeners = function () {
         var _this = this;
         this._cellManager.on('tornDownCell', function (circuitId) {
--- src/core/protocol/fileTransfer/Middleware.ts
+++ src/core/protocol/fileTransfer/Middleware.ts
@@ -6,15 +6,62 @@ import HydraMessageCenterInterface = require('../hydra/interfaces/HydraMessageCe
 import HydraNodeList = require('../hydra/interfaces/HydraNodeList');
 import HydraNode = require('../hydra/interfaces/HydraNode');
 
+/**
+ * MiddlewareInterface implementation.
+ *
+ * @class core.protocol.fileTransfer.Middleware
+ * @implements core.protocol.fileTransfer.MiddlewareInterface
+ *
+ * @param {core.protocol.hydra.CellManagerInterface} A working hydra cell manager.
+ * @param {core.protocol.net.ProtocolConnectionManagerInterface} A working protocol connection manager.
+ * @param {core.protocol.hydra.HydraMessageCenterInterface} A working hydra message center.
+ * @param {core.protocol.fileTransfer.WritableFileTransferMessageFactoryInterface} Factory for writable FILE_TRANSFER messages
+ */
 class Middleware implements MiddlewareInterface {
 
+	/**
+	 * Stores the hydra cell manager instance.
+	 *
+	 * @member {core.protocol.hydra.CellManagerInterface} core.protocol.fileTransfer.Middleware~_cellManager
+	 */
 	private _cellManager:CellManagerInterface = null;
-	private _protocolConnectionManager:ProtocolConnectionManagerInterface = null;
-	private _hydraMessageCenter:HydraMessageCenterInterface = null;
-	private _writableFileTransferMessageFactory:WritableFileTransferMessageFactoryInterface = null;
 
-	private _outgoingSockets:{[concatIdent:string]:string} = {};
+	/**
+	 * Stores the hydra message center.
+	 *
+	 * @member {core.protocol.hydra.HydraMessageCenterInterface} core.protocol.fileTransfer.Middleware~_hydraMessageCenter
+	 */
+	private _hydraMessageCenter:HydraMessageCenterInterface = null;
+
+	/**
+	 * Stores references to incoming middleware sockets assigned to the identifier of the circuit through which the messages should
+	 * be piped through.
+	 *
+	 * @member {Object} core.protocol.fileTransfer.Middleware~_incomingSockets
+	 */
 	private _incomingSockets:{[circuitId:string]:Array<string>} = {};
+
+	/**
+	 * Stores references to outgoing middleware sockets assigned to the concatenation of the fed node's attributes + the circuit
+	 * through which the EXTERNAL_FEED message came.
+	 *
+	 * @member {Object} core.protocol.fileTransfer.Middleware~_outgoingSockets
+	 */
+	private _outgoingSockets:{[concatIdent:string]:string} = {};
+
+	/**
+	 * Stores the protocol connection manager.
+	 *
+	 * @member {core.protocol.net.ProtcolConnectionManagerInterface} core.protocol.fileTransfer.Middleware~_protocolConnectionManager
+	 */
+	private _protocolConnectionManager:ProtocolConnectionManagerInterface = null;
+
+	/**
+	 * Stores the factory for writable FILE_TRANSFER messages
+	 *
+	 * @member {core.protocol.fileTransfer.WritableFileTransferMessageFactoryInterface} core.protocol.fileTransfer.Middleware~_writableFileTransferMessageFactory
+	 */
+	private _writableFileTransferMessageFactory:WritableFileTransferMessageFactoryInterface = null;
 
 	public constructor (cellManager:CellManagerInterface, protocolConnectionManager:ProtocolConnectionManagerInterface, hydraMessageCenter:HydraMessageCenterInterface, writableFileTransferMessageFactory:WritableFileTransferMessageFactoryInterface) {
 		this._cellManager = cellManager;
@@ -23,6 +70,21 @@ class Middleware implements MiddlewareInterface {
 		this._writableFileTransferMessageFactory = writableFileTransferMessageFactory;
 
 		this._setupListeners();
+	}
+
+	public addIncomingSocket (circuitId:string, socketIdentifier:string):void {
+		var existing:Array<string> = this._incomingSockets[circuitId];
+
+		if (existing) {
+			existing.push(socketIdentifier);
+		}
+		else {
+			this._incomingSockets[circuitId] = [socketIdentifier];
+		}
+	}
+
+	public closeSocketByIdentifier (socketIdentifier:string):void {
+		this._protocolConnectionManager.closeHydraSocket(socketIdentifier);
 	}
 
 	public feedNode (feedingNodes:HydraNodeList, associatedCircuitId:string, payloadToFeed:Buffer):void {
@@ -44,21 +106,56 @@ class Middleware implements MiddlewareInterface {
 		}
 	}
 
-	public addIncomingSocket (circuitId:string, socketIdentifier:string):void {
-		var existing:Array<string> = this._incomingSockets[circuitId];
+	/**
+	 * Self explanatory method. Used for assigning outgoing socket identifiers to nodes / circuits.
+	 *
+	 * @method core.protocol.fileTransfer.Middleware~_constructOutgoingKey
+	 *
+	 * @param {core.protocol.hydra.HydraNode} node The potential node to feed.
+	 * @param {string} circuitId The identifier of the circuit through which the EXTERNAL_FEED message came through
+	 * @returns {string} The concatenation used as identifier to assign a socket to.
+	 */
+	private _constructOutgoingKey(node:HydraNode, circuitId:string):string {
+		return circuitId + '_' + node.ip + '_' + node.port + '_' + node.feedingIdentifier;
+	}
 
-		if (existing) {
-			existing.push(socketIdentifier);
+	/**
+	 * Wraps a payload ina GOT_FED message, which again is wrapped in a FILE_TRANSFER message, and pipes it through
+	 * the TCP hydra socket stored under the given identifier (in the protocol connection manager).
+	 *
+	 * @method core.protocol.fileTransfer.Middleware~_feedSocket
+	 *
+	 * @param {string} socketIdentifier The TCP hydra socket to use.
+	 * @param {string} feedingIdentifier The feedingIdentifier of the node to feed. This is used as transferId for the FILE_TRANSFER message.
+	 * @param {Buffer} payloadToFeed The complete buffer of the message to feed.
+	 */
+	private _feedSocket (socketIdentifier:string, feedingIdentifier:string, payloadToFeed:Buffer):void {
+		var bufferToSend:Buffer = null;
+
+		try {
+			bufferToSend = this._hydraMessageCenter.wrapFileTransferMessage(this._writableFileTransferMessageFactory.constructMessage(feedingIdentifier, 'GOT_FED', payloadToFeed));
 		}
-		else {
-			this._incomingSockets[circuitId] = [socketIdentifier];
+		catch(e) {
+		}
+
+		if (bufferToSend) {
+			this._protocolConnectionManager.hydraWriteMessageTo(socketIdentifier, bufferToSend);
 		}
 	}
 
-	public closeSocketByIdentifier (socketIdentifier:string):void {
-		this._protocolConnectionManager.closeHydraSocket(socketIdentifier);
-	}
-
+	/**
+	 * Tries to open a TCP socket to one of the nodes of the provided list. This is done in the fashion described in
+	 * {@link core.protocol.fileTransfer.MiddlewareInterface}.
+	 * As soon as a connection has been established, the payload is fed to it.
+	 *
+	 * @method core.protocol.fileTransfer.Middleware~_obtainConnectionAndFeed
+	 *
+	 * @param {core.protocol.hydra.HydraNodeList} feedingNodes List of potential nodes to feed. The payload is of course, however, only fed to ONE node.
+	 * @param {string} associatedCircuitId The identifier of the circuit which the originating EXTERNAL_FEED message came through
+	 * @param {Buffer} payloadToFeed The payload to feed.
+	 * @param {Array<number>} usedIndices Optional, and only used internally if a follow-up call to this method must be performed. Indicates which nodes in the
+	 * list have already been probed.
+	 */
 	private _obtainConnectionAndFeed(feedingNodes:HydraNodeList, associatedCircuitId:string, payloadToFeed:Buffer, usedIndices:Array<number> = []):void {
 		var feedingNodesLength = feedingNodes.length;
 
@@ -87,24 +184,11 @@ class Middleware implements MiddlewareInterface {
 		}
 	}
 
-	private _feedSocket (socketIdentifier:string, feedingIdentifier:string, payloadToFeed:Buffer):void {
-		var bufferToSend:Buffer = null;
-
-		try {
-			bufferToSend = this._hydraMessageCenter.wrapFileTransferMessage(this._writableFileTransferMessageFactory.constructMessage(feedingIdentifier, 'GOT_FED', payloadToFeed));
-		}
-		catch(e) {
-		}
-
-		if (bufferToSend) {
-			this._protocolConnectionManager.hydraWriteMessageTo(socketIdentifier, bufferToSend);
-		}
-	}
-
-	private _constructOutgoingKey(node:HydraNode, circuitId:string):string {
-		return circuitId + '_' + node.ip + '_' + node.port + '_' + node.feedingIdentifier;
-	}
-
+	/**
+	 * Sets up the appropriate listeners on the cell manager and the protocol connection manager. Called in constructor.
+	 *
+	 * @method core.protocol.fileTransfer.Middleware~_setupListeners
+	 */
 	private _setupListeners ():void {
 		this._cellManager.on('tornDownCell', (circuitId:string) => {
 			var incomingSockets:Array<string> = this._incomingSockets[circuitId];
