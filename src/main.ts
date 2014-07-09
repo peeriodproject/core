1f4578b6f3ad15ebf24b394772cd44c6b0116c8b
Jörn Röder <me@joernroeder.de>
2014-07-09 16:55:56 +0200

    test lifetime

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,24 @@
-/**
- * Created by joernroeder on 4/21/14.
- */
-
 /// <reference path='./main.d.ts' />
 
 var gui = require('nw.gui');
+var logger = require('./core/utils/logger/LoggerFactory').create();
 
 import App = require('./core/App');
 
 App.start(gui.App.dataPath, gui.Window.get());
+
+// lifetime > 5 min < 1 day
+var minSeconds:number = 30;//300;
+var maxSeconds:number =120;//86400;
+
+var lifeTime = Math.max(minSeconds * 1000, Math.random() * maxSeconds * 1000);
+setTimeout(function () {
+	logger.info('quitting...');
+
+	return process.nextTick(function () {
+		gui.App.quit();
+	});
+}, lifeTime);
 
 
 /*import gui = require('nw.gui');
