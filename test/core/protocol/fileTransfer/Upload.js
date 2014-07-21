7cad216844f9b2beec8fb930f1fbc169574f3fb7
Jonathan Pirnay <mail@johnnycrab.com>
2014-07-21 15:00:26 +0200

    updates

---
+++ test/core/protocol/fileTransfer/Upload.js
@@ -0,0 +1,6 @@
+/// <reference path='../../../test.d.ts' />
+require('should');
+
+describe('CORE --> PROTOCOL --> FILE TRANSFER --> Upload (integration with Download) @current', function () {
+});
+//# sourceMappingURL=Upload.js.map
---
+++ test/core/protocol/fileTransfer/Upload.ts
@@ -0,0 +1,14 @@
+/// <reference path='../../../test.d.ts' />
+
+require('should');
+
+import events = require('events');
+import crypto = require('crypto');
+
+import sinon = require('sinon');
+
+import testUtils = require('../../../utils/testUtils');
+
+describe('CORE --> PROTOCOL --> FILE TRANSFER --> Upload (integration with Download) @current', function () {
+
+});
