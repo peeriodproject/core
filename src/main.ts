0d028fafeef3c30cea0ade5d8eabbe0677928481
Jörn Röder <kontakt@joernroeder.de>
2014-08-14 22:45:08 +0200

    fix

--- src/main.ts
+++ src/main.ts
@@ -35,11 +35,11 @@ var tray = new gui.Tray({
 /*menu.append(new gui.MenuItem({
     type: 'separator'
 }));*/
+var quitItem = new gui.MenuItem({
+	label: 'Quit'
+});
 
-menu.append(new gui.MenuItem({
-    label: 'Quit'
-}));
-menu.items[menu.items.length - 1].click = function() {
+quitItem.click = function() {
 	App.quit();
 
 	/*if (process.env.UI_ENABLED) {
@@ -51,5 +51,7 @@ menu.items[menu.items.length - 1].click = function() {
 		}, 40000);
 	}*/
 };
+
+menu.append(quitItem);
 
 tray.menu = menu;
